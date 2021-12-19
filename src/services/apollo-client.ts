444796c71d2cd81b7c4f01ac80973e61a40e74c7
Sander Paniago <sanderppaniago@gmail.com>
2021-12-18 21:05:19 -0400

    remove cache graphql

--- src/services/apollo-client.ts
+++ src/services/apollo-client.ts
@@ -1,37 +1,27 @@
-import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
 const fromBase64 = (value: string) => {
-    const buff = Buffer.from(value, 'base64')
-    return buff.toString('ascii')
+  const buff = Buffer.from(value, 'base64')
+  return buff.toString('ascii')
 }
 const httpLink = createHttpLink({
-    uri: "https://graphql.fauna.com/graphql",
+  uri: 'https://graphql.fauna.com/graphql'
 })
 
-
 const authLink = setContext((_, { headers }) => {
-    return {
-        headers: {
-            ...headers,
-            authorization: fromBase64(process.env.NEXT_PUBLIC_FAUNADB_KEY_GRAPHQL)
-        }
+  return {
+    headers: {
+      ...headers,
+      authorization: fromBase64(process.env.NEXT_PUBLIC_FAUNADB_KEY_GRAPHQL)
     }
+  }
 })
 
 const client = new ApolloClient({
-    ssrMode: typeof window === 'undefined',
-    link: authLink.concat(httpLink),
-    cache: new InMemoryCache({
-        typePolicies: {
-            allTransaciton: {
-                keyFields: ['']
-            },
-            userByEmail: {
-                keyFields: ['email']
-            }
-        }
-    }),
-});
+  ssrMode: typeof window === 'undefined',
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache()
+})
 
-export default client;
+export default client
